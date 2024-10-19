b96978d02a311f0fa96778c85869fb0774ba9671
carson fenimore <carson@takecontrolinc.com>
2024-10-19 15:38:23 +0000

    make cmake output relative to target

--- build.rs
+++ build.rs
@@ -14,18 +14,21 @@ fn submodules() -> PathBuf {
 fn main() {
     let submodules = submodules();
     let submodules_str = submodules.to_string_lossy();
+    let manifest = manifest_dir();
+    let manifest_str = manifest.to_string_lossy();
+    let output_str = format!("{manifest_str}/target/cmakeout");
 
-   let cmake_configure_result = Command::new("cmake")
-       .args(&["-S", &*format!("{submodules_str}/libcamlite"), "-B", &*format!("{submodules_str}/libcamlite/build")])
+    let cmake_configure_result = Command::new("cmake")
+       .args(&["-S", &*format!("{submodules_str}/libcamlite"), "-B", &*output_str])
        .status()
        .expect("Failed to execute CMake");
-   assert!(cmake_configure_result.success(), "CMake config failed");
+    assert!(cmake_configure_result.success(), "CMake config failed");
 
-   let cmake_build_result = Command::new("cmake")
-       .args(&["--build", &*format!("{submodules_str}/libcamlite/build")])
+    let cmake_build_result = Command::new("cmake")
+       .args(&["--build", &*output_str])
        .status()
        .expect("Failed to execute CMake --build");
-   assert!(cmake_build_result.success(), "CMake build failed");
+    assert!(cmake_build_result.success(), "CMake build failed");
 
     cxx_build::bridge("src/lib.rs")
         .file("cxx/wrap.cxx")
@@ -37,7 +40,7 @@ fn main() {
     println!("cargo:rustc-link-arg=/usr/local/lib/aarch64-linux-gnu/rpicam_app.so");
     println!("cargo:rustc-link-lib=camera");
     println!("cargo:rustc-link-lib=camera-base");
-    println!("{}", format!("cargo:rustc-link-search=native={submodules_str}/libcamlite/build"));
+    println!("{}", format!("cargo:rustc-link-search=native={output_str}"));
     println!("cargo:rustc-link-lib=dylib=camlite");
 
     println!("cargo:rerun-if-changed=cxx/wrap.h");
