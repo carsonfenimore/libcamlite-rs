4a16411a4a686b139ebcfc82ef2a8c2c7992058c
carson fenimore <carson@takecontrolinc.com>
2024-10-19 15:02:27 +0000

    Autobuild the libcamlite dependency

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
+
 
 fn manifest_dir() -> PathBuf {
     PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
@@ -10,14 +12,26 @@ fn submodules() -> PathBuf {
 }
 
 fn main() {
+    let submodules = submodules();
+    let submodules_str = submodules.to_string_lossy();
+
+   let cmake_configure_result = Command::new("cmake")
+       .args(&["-S", &*format!("{submodules_str}/libcamlite"), "-B", &*format!("{submodules_str}/libcamlite/build")])
+       .status()
+       .expect("Failed to execute CMake");
+   assert!(cmake_configure_result.success(), "CMake config failed");
+
+   let cmake_build_result = Command::new("cmake")
+       .args(&["--build", &*format!("{submodules_str}/libcamlite/build")])
+       .status()
+       .expect("Failed to execute CMake --build");
+   assert!(cmake_build_result.success(), "CMake build failed");
+
     cxx_build::bridge("src/lib.rs")
         .file("cxx/wrap.cxx")
         .std("c++20")
         .debug(true)
         .compile("rslibcamlite");
-
-    let submodules = submodules();
-    let submodules_str = submodules.to_string_lossy();
 
     println!("cargo:rustc-link-arg=/usr/lib/aarch64-linux-gnu/libboost_program_options.so.1.74.0");
     println!("cargo:rustc-link-arg=/usr/local/lib/aarch64-linux-gnu/rpicam_app.so");
